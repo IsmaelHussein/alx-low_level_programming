eb86a61e39de8abdd7f0ed3cb889014cf106adac
IsmaelHussein <ismaelhuz45@gmail.com>
2023-03-20 08:46:26 -0700

    t

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * Description main - Prints out a string to stdout.
+ *
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of an int: %d bytes(s)\n", sizeof(int));
+printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %d bytes(s)\n", sizeof(char));
+return (0);
+}
