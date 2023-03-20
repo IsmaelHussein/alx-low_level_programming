360a49bba66ec6065b4bfe69be76452499a5e6c9
IsmaelHussein <ismaelhuz45@gmail.com>
2023-03-20 09:04:32 -0700

    t

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Description main - Prints out a string to stdout.
+ * main Entry point
  *
- * Return: 0 if success.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
